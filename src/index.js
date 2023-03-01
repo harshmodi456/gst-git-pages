e11cf6d90a8185c0528c85c5ca5aa889d98b516f
khushbu <52320076+Moxa26@users.noreply.github.com>
2023-03-01 15:11:04 +0530

    add new code

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ import Store from "./Redux/Store/Store";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename="/WA-GST">
+    <BrowserRouter>
       <Provider store={Store}>
         <App />
       </Provider>
